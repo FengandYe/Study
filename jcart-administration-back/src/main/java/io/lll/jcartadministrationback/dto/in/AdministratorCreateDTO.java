9cb0b97660e59c3c6d9a00cd7970f8d24f806c27
liulinli <277797685@qq.com>
2020-02-24 18:02:33 +0800

    0224初始代码

---
+++ jcart-administration-back/src/main/java/io/lll/jcartadministrationback/dto/in/AdministratorCreateDTO.java
@@ -0,0 +1,67 @@
+package io.lll.jcartadministrationback.dto.in;
+
+public class AdministratorCreateDTO {
+    private String realName;
+    private String username;
+    private String email;
+    private String avatarUrl;
+    private String password;
+    private Byte status;
+    private String createTime;
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Byte getStatus() {
+        return status;
+    }
+
+    public void setStatus(Byte status) {
+        this.status = status;
+    }
+
+    public String getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(String createTime) {
+        this.createTime = createTime;
+    }
+}
---
+++ jcart-administration-back/src/main/java/io/lll/jcartadministrationback/dto/in/AdministratorUpdatePasswordDTO.java
@@ -0,0 +1,31 @@
+package io.lll.jcartadministrationback.dto.in;
+
+public class AdministratorUpdatePasswordDTO {
+    private Integer administratorId;
+    private String password1;
+    private String password2;
+
+    public Integer getAdministratorId() {
+        return administratorId;
+    }
+
+    public void setAdministratorId(Integer administratorId) {
+        this.administratorId = administratorId;
+    }
+
+    public String getPassword1() {
+        return password1;
+    }
+
+    public void setPassword1(String password1) {
+        this.password1 = password1;
+    }
+
+    public String getPassword2() {
+        return password2;
+    }
+
+    public void setPassword2(String password2) {
+        this.password2 = password2;
+    }
+}
